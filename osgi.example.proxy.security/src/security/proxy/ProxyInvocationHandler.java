a58d51c557eb2f5d9b99105827a066d6473765de
Christian Vogel <contact@christian-vogel.info>
2012-08-06 08:44:01 +0200

    Fixed #1 by adding additional methods for equals, hashcode and toString to avoid that they will be called on Object level

--- osgi.example.proxy.security/src/security/proxy/ProxyInvocationHandler.java
+++ osgi.example.proxy.security/src/security/proxy/ProxyInvocationHandler.java
@@ -25,7 +25,17 @@ public class ProxyInvocationHandler implements InvocationHandler, Serializable {
     }
 
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable { 
+    	String methodName = method.getName();
+    	
+    	if("equals".equals(methodName)) {
+    		return proxyEquals(proxy, args[0]);
+    	} else if("toString".equals(methodName)) {
+    		return proxyToString(proxy);
+    	} else if ("hashCode".equals(methodName)){
+    		return proxyHashCode(proxy);
+    	}
+    	
     	if(handler != null) {
     		handler.handle(method);
         }
@@ -34,6 +44,19 @@ public class ProxyInvocationHandler implements InvocationHandler, Serializable {
                 args);
 
         return invoke;
+    }
+    
+    protected Integer proxyHashCode(Object proxy) {
+    	return new Integer(System.identityHashCode(proxy));
+    }
+
+    protected Boolean proxyEquals(Object proxy, Object other) {
+    	return (proxy == other ? Boolean.TRUE : Boolean.FALSE);
+    }
+
+    protected String proxyToString(Object proxy) {
+    	return proxy.getClass().getName() + '@' +
+    	    Integer.toHexString(proxy.hashCode());
     }
 }
 
